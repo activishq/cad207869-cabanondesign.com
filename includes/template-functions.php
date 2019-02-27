3702ec3f47be17b44891552b7e677a4c74194aa0
Simon Bouchard <info@simonbouchard.com>
2019-02-27 10:48:58 -0500

    Code cleanup

--- includes/template-functions.php
+++ includes/template-functions.php
@@ -8,100 +8,6 @@
 *
 */
 
-
-/**
-* Header Layout
-*/
-if ( ! function_exists( 'activis_header_layout' ) ) {
-	function activis_header_layout() {
-
-		$options = activis_options();
-
-		if ( empty( $options['elementor_built_integration'] ) ) :
-
-			if ( $options['header_layout'] == '1') :
-				get_template_part( 'template-parts/layout/site-header', '1' );
-			elseif ( $options['header_layout'] == '2') :
-				get_template_part( 'template-parts/layout/site-header', '2' );
-			else :
-				get_template_part( 'template-parts/layout/site-header', '1' );
-			endif;
-
-		endif;
-
-	}
-}
-
-/**
-* Header Branding
-*/
-if ( ! function_exists( 'activis_header_brand' ) ) {
-	function activis_header_brand() {
-
-		$options = activis_options();
-
-		if ( ( isset( $options['logo']['url'] ) ) && ( trim( $options['logo']['url'] ) != "" ) ) :
-			$brand_logo = $options['logo']['url'];
-		?>
-		<a class="brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo( 'description' ); ?>">
-			<img src="<?php echo esc_url($brand_logo); ?>" alt="<?php bloginfo( 'name' ); ?>" />
-		</a>
-		<?php else : ?>
-		<a class="brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo( 'description' ); ?>">
-			<h1><?php bloginfo( 'name' ); ?></h1>
-		</a>
-		<?php endif;
-	}
-}
-
-/**
-* Mobile Navigation
-*/
-if ( ! function_exists( 'activis_mobile_navigation' ) ) {
-	function activis_mobile_navigation() {
-		get_template_part( 'template-parts/layout/mobile', 'navigation' );
-	}
-}
-
-/**
-* Loop
-*/
-if ( ! function_exists( 'activis_loop_before_layout' ) ) {
-	function activis_loop_before_layout() {
-		?>
-			<div class="grid-sizer"></div>
-		<?php
-	}
-}
-
-
-if ( ! function_exists( 'activis_loop_after_layout' ) ) {
-	function activis_loop_after_layout() {
-		?>
-
-		<?php
-	}
-}
-
-/**
-* Footer Layout
-*/
-if ( ! function_exists( 'activis_footer_layout' ) ) {
-	function activis_footer_layout() {
-
-		$options = activis_options();
-
-		if ( empty( $options['elementor_built_integration'] ) ) :
-			get_template_part( 'template-parts/layout/site', 'footer' );
-		endif;
-
-		if ( $options['general_backtotop'] == '1' ) { ?>
-			<a href="#0" class="ui-to-top"><?php _e('Top', 'activis'); ?></a>
-		<?php }
-
-	}
-}
-
 /**
 * Search Layout
 */
@@ -118,16 +24,6 @@ if ( ! function_exists( 'activis_search_layout' ) ) {
 
 	}
 }
-
-/**
-* 404 Layout
-*/
-if ( ! function_exists( 'activis_notfound_layout' ) ) {
-	function activis_notfound_layout() {
-		get_template_part( 'template-parts/layout/site', '404' );
-	}
-}
-
 
 /**
  * Google Analytics Code
--- includes/template-hooks.php
+++ includes/template-hooks.php
@@ -28,47 +28,6 @@ add_action( 'wp_head', 'activis_header_js', 99 );
  */
 add_action( 'activis_after_body', 'activis_ga_tag_js_body', 10 );
 add_action( 'elementor/page_templates/canvas/before_content', 'activis_ga_tag_js_body', 10 );
-add_action( 'activis_after_body', 'activis_mobile_navigation', 20 );
-
-
-/**
- * Site Header
- *
- * @see template-functions -> activis_header_layout()
- * @see template-functions -> activis_header_brand()
- *
- */
-add_action( 'activis_site_header', 'activis_header_layout', 10 );
-add_action( 'activis_header_branding', 'activis_header_brand', 10 );
-
-/**
- * Page
- *
- * @see template-functions -> activis_page_header
- * @see template-functions -> activis_404_layout
- * @see template-functions -> activis_search_layout
- *
- */
-add_action( 'activis_page_notfound', 'activis_notfound_layout', 10 );
-add_action( 'activis_page_search', 'activis_search_layout', 10 );
-
-/**
- * Loop
- *
- * @see template-tags -> activis_paging_nav
- *
- */
-add_action( 'activis_loop_before', 'activis_loop_before_layout', 10 );
-add_action( 'activis_loop_after', 'activis_loop_after_layout', 10 );
-add_action( 'activis_loop_after', 'activis_paging_nav', 10 );
-
-/**
- * Site Footer
- *
- * @see template-functions -> activis_footer_layout()
- *
- */
-add_action( 'activis_site_footer', 'activis_footer_layout', 10 );
 
 /**
  * Footer

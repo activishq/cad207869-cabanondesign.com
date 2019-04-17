fff30ded1125ba566bb1f3efdf900bffbb4d251a
Frederic H-F <frederic@hfournier.com>
2019-04-17 15:17:06 -0400

    Feature : Dynamic TagManager

--- _source/js/app.js
+++ _source/js/app.js
@@ -42,6 +42,7 @@
 		init: function() {
 
 			app.dynamicform();
+			app.tagmanager();
 
 		},
 
@@ -79,6 +80,107 @@
 				});
 			});
 		},
+
+		tagmanager: function() {
+			if( $('.tagmanager-click').length > 0 ){
+				$('.tagmanager-click').each(function() {
+					var tagmanagerTarget = $(this).data('tagmanager-target');
+					var tagmanagerCategory = $(this).data('tagmanager-category');
+					var tagmanagerAction = $(this).data('tagmanager-action');
+					var tagmanagerLabel = $(this).data('tagmanager-label');
+
+					if( tagmanagerTarget != '' && tagmanagerCategory != '' && tagmanagerAction != '' && tagmanagerLabel != '' ){
+						if( $(this).find(tagmanagerTarget).length > 0 ){
+							$(this).find(tagmanagerTarget).click(function(e){
+								dataLayer.push({
+									'event': 'gaEvent',
+									'eventCategory' : tagmanagerCategory,
+									'eventAction': tagmanagerAction,
+									'eventLabel': tagmanagerLabel
+								});
+							});
+						}
+					}
+				});
+			}
+
+			var activis_submitControllerEvent = Marionette.Object.extend( {
+
+				initialize: function() {
+					this.listenTo( Backbone.Radio.channel( 'forms' ), 'submit:response', this.actionSubmit );
+				},
+
+				actionSubmit: function( response ) {
+					if( response[ 'data' ][ 'form_id' ] == '1' ){
+						dataLayer.push({
+							'event': 'gaEvent',
+							'eventCategory' : 'Form',
+							'eventAction': 'Submit',
+							'eventLabel': 'Contact Me'
+						});
+					}
+					else if( response[ 'data' ][ 'form_id' ] == '2' ){
+						dataLayer.push({
+							'event': 'gaEvent',
+							'eventCategory' : 'Form',
+							'eventAction': 'Submit',
+							'eventLabel': 'Formulaire de soumission'
+						});
+					}
+					else if( response[ 'data' ][ 'form_id' ] == '3' ){
+						dataLayer.push({
+							'event': 'gaEvent',
+							'eventCategory' : 'Form',
+							'eventAction': 'Submit',
+							'eventLabel': 'Demande de rendez-vous'
+						});
+					}
+					else if( response[ 'data' ][ 'form_id' ] == '4' ){
+						dataLayer.push({
+							'event': 'gaEvent',
+							'eventCategory' : 'Form',
+							'eventAction': 'Submit',
+							'eventLabel': 'Devenir un distributeur'
+						});
+					}
+					else if( response[ 'data' ][ 'form_id' ] == '5' ){
+						dataLayer.push({
+							'event': 'gaEvent',
+							'eventCategory' : 'Form',
+							'eventAction': 'Submit',
+							'eventLabel': 'Demande de rendez-vous - Page'
+						});
+					}
+					else if( response[ 'data' ][ 'form_id' ] == '7' ){
+						dataLayer.push({
+							'event': 'gaEvent',
+							'eventCategory' : 'Form',
+							'eventAction': 'Submit',
+							'eventLabel': 'Request a quote'
+						});
+					}
+					else if( response[ 'data' ][ 'form_id' ] == '9' ){
+						dataLayer.push({
+							'event': 'gaEvent',
+							'eventCategory' : 'Form',
+							'eventAction': 'Submit',
+							'eventLabel': 'Book your home appointment'
+						});
+					}
+					else if( response[ 'data' ][ 'form_id' ] == '11' ){
+						dataLayer.push({
+							'event': 'gaEvent',
+							'eventCategory' : 'Form',
+							'eventAction': 'Submit',
+							'eventLabel': 'Become a distributor'
+						});
+					}
+				},
+
+			});
+
+			new activis_submitControllerEvent();
+		}
 
 	};
 
